23e04c3d644f644babfcf63699e389e2c5a56e92
malikwillis <jmebben8@icloud.com>
2026-05-01 11:23:04 -0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.RUL_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAodFZJa7T7fxLdSZ4evm_eBvzcKxU7MYA",
-  authDomain: "rul-upvote-pickem.firebaseapp.com",
-  projectId: "rul-upvote-pickem",
-  storageBucket: "rul-upvote-pickem.firebasestorage.app",
-  messagingSenderId: "1092312036790",
-  appId: "1:1092312036790:web:72d873514ebd326d58fd17"
-};
