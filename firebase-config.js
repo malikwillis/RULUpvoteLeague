9c38b117c93e850d39ebf96ce79e4ab3424b97fb
malikwillis <jmebben8@icloud.com>
2026-05-01 10:40:37 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.RUL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA5wt3c5GPszFVcgIaXI6azQBVo8iGTXuY",
+  authDomain: "rul-pickem.firebaseapp.com",
+  projectId: "rul-pickem",
+  storageBucket: "rul-pickem.firebasestorage.app",
+  messagingSenderId: "199067733259",
+  appId: "1:199067733259:web:567e5e0a761259ca5c660e"
+};
